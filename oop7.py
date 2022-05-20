ba5e1f51c2944246c24306f1ae991f978e74ea24
giridhar276 <giridhar276@gmail.com>
2022-05-20 14:48:44 +0530

    Add files via upload

---
+++ oop7.py
@@ -0,0 +1,22 @@
+
+
+class Date:   # Parent # Base # Super
+    def get_date(self):
+        print("2016-05-14")
+
+
+
+class Time(Date): # Child # inherited
+    def get_time(self):
+        print("08:00:00")
+
+# Creating an instance from `Date`
+dt = Date()
+dt.get_date()  # Accesing the `get_date()` method of `Date`
+print("--------")
+
+# Creating an instance from `Time`.
+tm = Time()
+tm.get_time()   # Accessing the `get_time()` method from `Time`.
+# Accessing the `get_date() which is defined in the parent class `Date`.
+tm.get_date()
